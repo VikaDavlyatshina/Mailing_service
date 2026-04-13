6071e53657edbf780bf1864c173291b4bef1537f
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-04-14 01:42:35 +0300

    Настроена админка для отображения пользователей

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import CustomUser
 
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """
+    Настройка отображения кастомной модели пользователя в админке.
+    """
+
+    # Поля, отображаемые в списке пользователей
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+
+    # Фильтры в правой боковой панели
+    list_filter = ("is_staff", "is_active", "is_superuser", "date_joined")
+
+    # Поля для поиска
+    search_fields = ("email", "username", "first_name", "last_name")
+
+    # Количество записей на странице
+    list_per_page = 25
+
+    # Поля, которые можно редактировать прямо в списке
+    list_editable = ("is_active",)
+
+    # Порядок сортировки по умолчанию
+    ordering = ("-date_joined",)
